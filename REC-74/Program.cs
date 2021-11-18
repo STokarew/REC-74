127da40cbb985781c90b43e4cf2e5e77cc2cea89
сергей токарев <sseregatokarew@gmail.com>
2021-11-18 15:08:06 +0700

    Добавьте файлы проекта.

---
+++ REC-74/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace REC_74
+{
+    class Program
+    {
+        static float MineSum(float number1, float number2)
+        {
+            return number1 + number2;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое из суммируемых чисел: ");
+            var Num1 = float.Parse(Console.ReadLine());
+            Console.Write("Введите второе из суммируемых чисел: ");
+            var Num2 = float.Parse(Console.ReadLine());
+            Console.Write($"Сумма введенных чисел: {MineSum(Num1, Num2)} ");
+            Console.ReadKey();
+        }
+    }
+}
